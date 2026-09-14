fc58cb4b630cc4c5e220a7b86981ad0e9c97a1da
aosisdaniel-work <aosisdaniel@gmail.com>
2026-09-14 10:36:39 +0200

    Learning about math and devision and variables. Also adding comments.

--- numbers-and-int.py
+++ numbers-and-int.py
@@ -1,3 +1,4 @@
+#Learning about numbers and variables
 question1 = 2+2
 question2 = 50-5*6
 question3 = (50-5*6)/4
@@ -6,3 +7,15 @@ print(question1)
 print(question2)
 print(question3)
 print(question4)
+print("\n")
+
+#Learning about div and remainders
+question5 = 17/3
+question6 = 17//3
+question7 = 17%3
+question8 = 5*3+2
+print(question5)
+print(question6)
+print(question7)
+print(question8)
+print("\n")
