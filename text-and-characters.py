384eb681bbfd6497d2d695aa309c49a11987b08c
aosisdaniel-work <aosisdaniel@gmail.com>
2026-09-21 13:56:56 +0200

    Starting with text and characters, we can explore how to manipulate strings in Python.

---
+++ text-and-characters.py
@@ -0,0 +1,16 @@
+#text lines#
+print("This is a text line")
+print("Paris rabbit has your back :) !")
+print('testing single quotes')
+
+#Single quotes and special characters#
+print('doesn\'t')
+print("\'Yes,\' they said")
+print('"Isn\'t," they said')
+
+#variable and text lines#
+s = "This is a text line.\nThis is another text line."
+print(s)
+#directory path#
+print('C:\this\name')
+print(r'C:\some\name')
